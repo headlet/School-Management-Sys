3a11f481d23266c6e326a13f95fa0617369e550b
headlet <professionalskill6@gmail.com>
2025-12-05 16:25:42 +0545

    changing the class logics and frontend

---
+++ resources/views/admin/classes/viewclass.blade.php
@@ -0,0 +1,10 @@
+@extends("admin.index")
+@section('content')
+<section>
+<div class="w-full flex flex-col sm:flex-row justify-between items-start sm:items-center gap-3 sm:gap-0 min-h-14 sm:h-14 border border-gray-300 rounded-lg shadow-md px-3 sm:px-4 py-3 sm:py-0 mb-4 sm:mb-6 bg-white">
+        <h2 class="text-base sm:text-lg font-semibold text-gray-800">All Class</h2>
+
+    </div>
+
+</section>
+@endsection
